d45dccbcacb271e383bc83dec32f9f926bd49777
Bas Terwijn <bterwijn@gmail.com>
2020-05-08 00:19:31 +0200

    update

--- src/VirusGame.cpp
+++ src/VirusGame.cpp
@@ -8,21 +8,24 @@
 const int SCREEN_FPS = 60;
 const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
+/*! @file
+   The starting point of VirusGame, holds the main() function.
+*/
 extern "C" int main(int argc, char* argv[])
 {
-    srand(time(NULL)); // seed speudo random number generator 
-    MySDL mySDL("VirusGame"); // create DSL graphics window
+    srand(time(NULL));        // seed the speudo random number generator 
+    MySDL mySDL("VirusGame"); // create an SDL graphics window
     
     Player player(mySDL.size()*0.5); // player
 
-    const int max_nr_units=20; // the evil viruses
-    int nr_units=0;
-    Virus units[max_nr_units];
-    double new_virus_chance=0.01;
-    double new_virus_max_speed=3;
+    const int max_nr_units=20;    // maximum number of units, feel free to change
+    int nr_units=0;               // current number of units
+    Virus units[max_nr_units];    // holds all units in the game
+    double new_virus_chance=0.01; // the chance that a new virus is created each time step
+    double new_virus_max_speed=3; // maximum speed of a new virus
     
     bool quit=false;
-    while (!quit) // game loop
+    while (!quit) // the game loop
     {
         int ticks_start=SDL_GetTicks();
         SDL_SetRenderDrawColor(mySDL.renderer(),0,0,0,255); // black background
@@ -44,17 +47,15 @@ extern "C" int main(int argc, char* argv[])
         }
         const Uint8* keyboardState=SDL_GetKeyboardState(NULL); // get keyboard state
 
-        player.keyboard(keyboardState); // handle player
+        player.keyboard(keyboardState); // control the player by keyboard
         player.step(mySDL);
         player.draw(mySDL);
         
-        if (rand_0_1()<new_virus_chance && nr_units<max_nr_units) // create new virus
+        if (rand_0_1()<new_virus_chance && nr_units<max_nr_units) // by chance create new virus
         {
             Coord window_size=mySDL.size();
-            Coord pos=Coord(window_size.x * rand_0_1(),
-                            window_size.y * rand_0_1());
-            Coord speed=Coord(new_virus_max_speed * rand_m1_1(),
-                              new_virus_max_speed * rand_m1_1());
+            Coord pos  = Coord(window_size.x       * rand_0_1() ,  window_size.y       * rand_0_1() );
+            Coord speed= Coord(new_virus_max_speed * rand_m1_1(),  new_virus_max_speed * rand_m1_1());
             units[nr_units]=Virus(pos,speed);
             nr_units+=1;
         }
@@ -70,5 +71,3 @@ extern "C" int main(int argc, char* argv[])
     }
     return 0;
 }
-
-
